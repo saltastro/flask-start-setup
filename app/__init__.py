8a1d72be2c0cd5fdf77e06796e1bb74b2bd6d115
Christian Hettlage <hettlage@saao.ac.za>
2016-08-23 10:23:46 +0200

    Clearer definition of SSL status

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sslify import SSLify
 
-from config import config
+from config import config, SSLStatus
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+    config[config_name].init_app(app, 'development')
 
-    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+    if not app.debug and not app.testing and app.config['SSL_STATUS'] == SSLStatus.ENABLED:
         sslify = SSLify(app)
 
     from .main import main as main_blueprint
