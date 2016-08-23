61763b94486bbf576fe9475987cb00dba81c1932
Christian Hettlage <hettlage@saao.ac.za>
2016-08-23 10:22:50 +0200

    More illustration of error handling

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,6 +1,19 @@
+from flask import current_app
+
 from . import main
 
 
 @main.errorhandler(500)
 def internal_server_error(e):
-    return 'Replace me with a proper error.'
+    return 'Replace me with proper error handling.'
+
+
+@main.errorhandler(404)
+def file_not_found_error(e):
+    return 'Replace me with proper error handling.'
+
+
+@main.errorhandler(Exception)
+def exception_raised(e):
+    current_app.logger.error(str(e))
+    return 'Replace me with proper error handling.'
