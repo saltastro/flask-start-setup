cdc3f8e4d518c0beccd70f7891b11b38af19bf11
Christian Hettlage <hettlage@saao.ac.za>
2016-09-02 21:07:04 +0200

    Tornado server

---
+++ tornado_server.py
@@ -0,0 +1,11 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
+from app import create_app
+
+app = WSGIContainer(create_app('production'))
+
+http_server = HTTPServer(app)
+http_server.listen(8080)
+IOLoop.instance().start()
