73265cc06fe6b862701b2ac8e5922e8dc1f702f2
Christian Hettlage <hettlage@saao.ac.za>
2016-09-05 21:49:39 +0200

    Removed commands from manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
 import os
-COV = None
-if os.environ.get('FLASK_COVERAGE'):
-    import coverage
-    COV = coverage.coverage(branch=True, include='app/*')
-    COV.start()
-
-if os.path.exists('.env'):
-    print('Importing environment from .env...')
-    for line in open('.env'):
-        var = line.strip().split('=')
-        if len(var) == 2:
-            os.environ[var[0]] = var[1]
 
 from app import create_app
 from flask_script import Manager, Shell
@@ -26,28 +14,6 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @manager.command
-def test(coverage=False):
-    """Run the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE'):
-        import sys
-        os.environ['FLASK_COVERAGE'] = '1'
-        os.execvp(sys.executable, [sys.executable] + sys.argv)
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
-
-
-@manager.command
 def profile(length=25, profile_dir=None):
     """Start the application under the code profiler."""
     from werkzeug.contrib.profiler import ProfilerMiddleware
@@ -57,8 +23,13 @@ def profile(length=25, profile_dir=None):
 
 
 @manager.command
+def test():
+    raise NotImplementedError('Please use the command "./run_tests.sh" for running the tests.')
+
+@manager.command
 def deploy():
-    pass
+    raise NotImplementedError('Please use the command "fab setup" for setting up the server and the command '
+                              '"fab deploy" for deploying code afterwards.')
 
 if __name__ == '__main__':
     manager.run()
