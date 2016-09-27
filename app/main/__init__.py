07ef9edbd33ef9e45950f1331c25a44482107f1e
Christian Hettlage <hettlage@saao.ac.za>
2016-09-27 13:18:05 +0200

    Load Bokeh resources

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,12 @@
+from bokeh.resources import CDN
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
+
+
+@main.context_processor
+def bokeh_resources():
+    return dict(bokeh_resources=CDN)
+
 
 from . import views, errors
